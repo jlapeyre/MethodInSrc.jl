c62b2782087344b1589c0e8edae34d2e4c4684a0
John Lapeyre <jlapeyre@users.noreply.github.com>
2024-09-29 16:47:50 -0400

    Disable JET test for Julia v1.6
    JET fails with Julia v1.6. Really hard to understand what the error means
    or even where it is supposedly occuring.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using MethodInSrc
 using Test
 
-include("jet_test.jl")
+# Some incomprehensible failure with JET
+# on Julia v 1.6
+if VERSION >= v"1.7"
+    include("jet_test.jl")
+end
 include("aqua_test.jl")
 include("method_in_src.jl")
